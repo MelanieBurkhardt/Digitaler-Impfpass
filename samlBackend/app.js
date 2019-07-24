d380fdfb0c2658083f9b9975ae6243d54d9e235d
MelanieBurkhardt <burkhardt-melanie@outlook.de>
2019-07-24 15:19:34 +0200

    Inserted MongoDB features

--- samlBackend/app.js
+++ samlBackend/app.js
@@ -21,8 +21,6 @@ const flash = require('connect-flash');
 
 const app = express();
 
-var passport = require('passport');
-var samlStrategy = require('passport-saml').Strategy;
 // Keycloack
 var session = require('express-session');
 var Keycloak = require('keycloak-connect');
@@ -39,6 +37,78 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
 	done(null, user);
 });
+
+// TODO: Insert MongoDB content from mongoDBconnection/app.js here
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// // Express body parser
+// app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// // // Passport middleware
+// // app.use(passport.initialize());
+// // app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+//path changed. old: '/dashboard/vaccinations ???CHECK???
+app.use('/dashboard/vaccinations', require('./routes/vaccinations.js'));
+console.log('xyz');
+//get methode for icall - app.use or app.get?? 
+//app.use('/calendar', require('./routes/calendar.js'));
+//app.get
+/*
+app.use('/', (req, res) => {console.log('hallo');
+  // for demo purpose just create iCal on the fly
+  let iCal = ics.createNew('iCal Demo by Shaun Calendar', null, null, null, '-//Shaun Xu//NONSGML iCal Demo Calendar//EN');
+  let iCalString = iCal.toICSString();
+
+  res.set('Content-Type', 'text/calendar;charset=utf-8');
+  res.set('Content-Disposition', 'attachment; filename="worktile.pro.calendar.my.ics"');
+  res.send(iCalString);
+}); */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
 
 var samlStrategy = new saml.Strategy({
 	// URL that goes from the Identity Provider -> Service Provider
@@ -195,4 +265,3 @@ var server = app.listen(4006, function () {
 });
 
 
-// TODO: Insert MongoDB content from mongoDBconnection/app.js here
